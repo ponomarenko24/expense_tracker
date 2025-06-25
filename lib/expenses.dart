6facf19854c54b1993d12b14429e679726ac508c
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-06-25 13:25:08 +0300

    added expenses.dart with statefulwidget

---
+++ lib/expenses.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(children: [Text('The chart'), Text('Expenses list...')]),
+    );
+  }
+}
