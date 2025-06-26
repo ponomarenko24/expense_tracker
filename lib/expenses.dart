5ce2422d489e0e12bc18e5fed27fc74535c962fa
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-06-27 00:01:47 +0300

    added list inside of other list via Expanded

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expence_tracker/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expence_tracker/models/expense.dart';
 
@@ -8,15 +9,29 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-
   final List<Expense> _registedExpenses = [
-    Expense(title: 'Flutter courses', amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure)
+    Expense(
+      title: 'Flutter courses',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
   ];
   @override
   Widget build(context) {
     return Scaffold(
-      body: Column(children: [Text('The chart'), Text('Expenses list...')]),
+      body: Column(
+        children: [
+          Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registedExpenses)),
+        ],
+      ),
     );
   }
 }
