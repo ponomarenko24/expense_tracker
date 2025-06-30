e0611d9d844085902ab75f998e1985321b64a8e1
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-06-30 21:21:19 +0300

    added new_expense file with textfield

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expence_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expence_tracker/widgets/expenses_list/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expence_tracker/models/expense.dart';
 
@@ -27,7 +28,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('Modal bottom sheet'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
