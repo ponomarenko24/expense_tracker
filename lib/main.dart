b8e1a35385aef0ca2ef3cb696bd8a83e8a3b4741
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-07-03 19:32:15 +0300

    added color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:expence_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(
-    home: const Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
