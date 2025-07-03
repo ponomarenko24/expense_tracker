545fb5e944ee5fa5b750f24ce981c90162269fec
Volodymyr Ponomarenko <vladimir.ponomarenko0924@gmail.com>
2025-07-03 20:28:16 +0300

    edited text theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,22 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          )
+        )
       ),
       home: const Expenses(),
     ),
